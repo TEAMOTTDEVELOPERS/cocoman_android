945011cf037535d02936ad85052aa798dc9a7b86
cindia3704 <cindia3704@gmail.com>
2021-01-13 23:40:33 +0900

    make navigation bar and connect to fragments

--- CoCoMan/app/src/main/java/com/example/cocoman/activity/MainActivity.kt
+++ CoCoMan/app/src/main/java/com/example/cocoman/activity/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.RelativeLayout
 import com.example.cocoman.R
+import com.example.cocoman.adapter.NavigationTabAdapter
+import com.google.android.material.tabs.TabLayout
 //import com.example.cocoman.adapter.NavigationTabAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,14 +14,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        nav_bar.addTab(nav_bar.newTab().setText("홈"))
+//        nav_bar.addTab(nav_bar.newTab().setText("검색"))
+//        nav_bar.addTab(nav_bar.newTab().setText("추천"))
+//        nav_bar.addTab(nav_bar.newTab().setText("그룹"))
+//        nav_bar.addTab(nav_bar.newTab().setText("프로필"))
+        makeBottomNavigation()
+        fragment_page.setCurrentItem(0)
+
     }
-//
-//    private fun bottomNavigation(){
-//        fragment_page.adapter = NavigationTabAdapter(supportFragmentManager,5)
-//        nav_bar.setupWithViewPager(fragment_page)
-//
-//        val bottomTab : View = this.layoutInflater.inflate(R.layout.navigation_tab,null,false)
-//
-//        nav_bar.getTabAt(0)!!.customView = bottomTab.findViewById(R.id.home_tab) as RelativeLayout
-//    }
+    private fun makeBottomNavigation(){
+        val pagerAdapter = NavigationTabAdapter(supportFragmentManager,5)
+        fragment_page.adapter = pagerAdapter
+        nav_bar.setupWithViewPager(fragment_page)
+
+        val navigationLayout: View = this.layoutInflater.inflate(R.layout.navigation_tab,null,false)
+        nav_bar.getTabAt(0)!!.customView=navigationLayout.findViewById(R.id.home_tab) as RelativeLayout
+        nav_bar.getTabAt(1)!!.customView=navigationLayout.findViewById(R.id.search_tab) as RelativeLayout
+        nav_bar.getTabAt(2)!!.customView=navigationLayout.findViewById(R.id.recommend_tab) as RelativeLayout
+        nav_bar.getTabAt(3)!!.customView=navigationLayout.findViewById(R.id.group_tab) as RelativeLayout
+        nav_bar.getTabAt(4)!!.customView=navigationLayout.findViewById(R.id.profile_tab) as RelativeLayout
+    }
+
 }
--- CoCoMan/app/src/main/java/com/example/cocoman/adapter/NavigationTabAdapter.kt
+++ CoCoMan/app/src/main/java/com/example/cocoman/adapter/NavigationTabAdapter.kt
@@ -3,19 +3,20 @@ package com.example.cocoman.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.example.cocoman.fragment.*
 
-//class NavigationTabAdapter (fm:FragmentManager,val fragmentCount:Int): FragmentStatePagerAdapter(fm){
-//    override fun getCount(): Int {
-//        return fragmentCount
-//    }
+class NavigationTabAdapter (fm:FragmentManager,val fragmentCount:Int): FragmentStatePagerAdapter(fm){
+    override fun getCount(): Int {
+        return fragmentCount
+    }
 
-//    override fun getItem(position: Int): Fragment {
-//        when(position){
-//            0 -> return HomeFragment.newInstance()
-//            1 -> return HomeFragment.newInstance()
-//            2 -> return HomeFragment.newInstance()
-//            3 -> return HomeFragment.newInstance()
-//            else -> return HomeFragment.newInstance()
-//        }
-//    }
-//}
+    override fun getItem(position: Int): Fragment {
+        when(position){
+            0 -> return HomeFragment()
+            1 -> return SearchFragment()
+            2 -> return OTTRecommendFragment()
+            3 -> return GroupFragment()
+            else -> return MyProfileFragment()
+        }
+    }
+}
