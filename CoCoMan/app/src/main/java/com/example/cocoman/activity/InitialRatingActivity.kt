f845ff32d5ec57d46c3f2fca28ce7fb68904bbe6
cindia3704 <cindia3704@gmail.com>
2021-01-13 15:49:41 +0900

    initial rating add star animation

--- CoCoMan/app/src/main/java/com/example/cocoman/activity/InitialRatingActivity.kt
+++ CoCoMan/app/src/main/java/com/example/cocoman/activity/InitialRatingActivity.kt
@@ -3,16 +3,15 @@ package com.example.cocoman.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
-import android.widget.AbsListView
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cocoman.R
 import com.example.cocoman.adapter.ContentRatingAdaptor
 import com.example.cocoman.data.ContentRating
 import kotlinx.android.synthetic.main.activity_initial_rating_acitivity.*
+import kotlinx.android.synthetic.main.rating_content_view.*
 
 class InitialRatingActivity : AppCompatActivity() {
     lateinit var totalRated : TextView
@@ -20,37 +19,81 @@ class InitialRatingActivity : AppCompatActivity() {
     lateinit var doneBtn: Button
     var rateCompleted:Int = 0
     var shouldBeRated : Int = 10-rateCompleted
+    val contentList = ArrayList<ContentRating>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 맨 처음 뷰 만들때
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initial_rating_acitivity)
         initView()
-        doneBtn.setOnClickListener {
-            val intent = Intent(this@InitialRatingActivity, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+        setupListener()
+        getData()
         layoutInit()
+    }
 
+    override fun onResume() {
+        // 새로고침
+        super.onResume()
+        layoutInit()
+        checkStars()
+    }
+    fun getData(){
+        // TODO: 서버로부터 데이터 가져오기
+        for ( i in 0 until 10){
+            contentList.add(ContentRating("Harry Potter 2",2012,0.0F))
+        }
     }
     fun layoutInit(){
-        val contentList = ArrayList<ContentRating>()
-        for ( i in 0 until 10){
-            contentList.add(ContentRating("Harry Potter 2",2012,1))
-        }
         val adapter = ContentRatingAdaptor(contentList, LayoutInflater.from(this@InitialRatingActivity))
         content_recylerview.adapter=adapter
         content_recylerview.layoutManager = LinearLayoutManager(this@InitialRatingActivity)
     }
+    fun setupListener(){
+        doneBtn.setOnClickListener {
+            if(shouldBeRated<=0) {
+                val intent = Intent(this@InitialRatingActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else{
+                Toast.makeText(this@InitialRatingActivity,""+shouldBeRated+"개를 더 평가해주세요.",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
     fun initView(){
+        // lateinit 으로 만든거 xml 에서 요소 찾아주기
         totalRated = findViewById(R.id.rating_totalNum)
         rateComment = findViewById(R.id.rating_ment)
         doneBtn = findViewById(R.id.ok_rating)
     }
 
+    fun checkStars(){
+        var completed = 0
+        for( i in 0 until 10){
+            Log.d("rate","completed:"+i+": "+contentList[i].score)
+           if(contentList[i].score!=0.0F){
+               completed += 1
+               Log.d("rate","completed:"+completed)
+           }
+        }
+        if(completed > rateCompleted){
+            for(i in 0 until completed) {
+                incrementRated()
+            }
+        }
+        if(completed<rateCompleted){
+            for(i in 0 until (rateCompleted-completed)) {
+                decrementRated()
+            }
+
+        }
+    }
+
+
     fun incrementRated(){
         rateCompleted+=1
+        shouldBeRated = 10-rateCompleted
         totalRated.setText(rateCompleted.toString())
         if(rateCompleted>=10){
             rateComment.setText("10개 달성!")
@@ -61,6 +104,7 @@ class InitialRatingActivity : AppCompatActivity() {
 
     fun decrementRated(){
         rateCompleted-=1
+        shouldBeRated = 10-rateCompleted
         totalRated.setText(rateCompleted.toString())
         if(rateCompleted>=10){
             rateComment.setText("10개 달성!")
--- CoCoMan/app/src/main/java/com/example/cocoman/adapter/ContentRatingAdaptor.kt
+++ CoCoMan/app/src/main/java/com/example/cocoman/adapter/ContentRatingAdaptor.kt
@@ -1,11 +1,15 @@
 package com.example.cocoman.adapter
 
 import android.app.Activity
+import android.media.Rating
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.cocoman.R
@@ -17,19 +21,21 @@ import kotlinx.android.synthetic.main.rating_content_view.view.*
 class ContentRatingAdaptor (
     val contentList: ArrayList<ContentRating>,
     val inflater: LayoutInflater
+
 ):RecyclerView.Adapter<ContentRatingAdaptor.ViewHolder>(){
     class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
         val contentTitle: TextView
         val contentYear: TextView
-        val contentScore: TextView
+        val contentScore: RatingBar
         val contentPoster: ImageView
 
         init {
             contentTitle=itemView.findViewById(R.id.content_title)
             contentYear = itemView.findViewById(R.id.content_year)
-            contentScore = itemView.findViewById(R.id.content_rating)
+            contentScore = itemView.findViewById(R.id.content_star)
             contentPoster = itemView.findViewById(R.id.content_poster)
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -44,11 +50,18 @@ class ContentRatingAdaptor (
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.contentTitle.setText(contentList.get(position).title)
         holder.contentYear.setText(contentList.get(position).year.toString())
-        holder.contentScore.setText(contentList.get(position).score.toString())
+        holder.contentScore.rating=contentList.get(position).score
+        holder.contentScore.setOnRatingBarChangeListener(object : RatingBar.OnRatingBarChangeListener{
+            override fun onRatingChanged(p0: RatingBar?, p1: Float, p2: Boolean) {
+                contentList[position].score = p1
+                Log.d("rate",""+position+":"+contentList[position].score)
 
+            }
+        })
 //        Glide.with(activity)
 //            .load(holder.contentPoster)
 //            .placeholder(R.drawable.harrypotterposter)
 //            .into(holder.contentPoster)
     }
+
 }
